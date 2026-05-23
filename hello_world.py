8b223f3c2822f79ed6e7f666cf29eab3e613842e
Emmi <emmicronje@gmail.com>
2026-05-23 15:12:14 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#Request input from user to input their name
+#Store input into variable "name" and print it out
+name = input("Please enter your name: ")
+print(name)
+
+#Request input from user to input ther age
+#Store input into variable "age" and print it out
+age = input("Please enter your age: ")
+print(age)
+#Print "Hello, World" on a new line
+print()
+print("Hello, World!")
